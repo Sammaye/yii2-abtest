85e70ff609893599ed5004417c83f793600b45b4
Sam Millman <sam.millman@gmail.com>
2016-11-03 14:53:11 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # yii2-abtest
-A simple extension for writing A/B Tests in Yii2
+A simple extension for writing A/B Tests in Yii2.
+
+This extension does not include the hooks for various programs, but does allow you to `list()` so you can quickly 
+add it to your analytics code.
+
+## Installing
+
+Simply include it from composer:
+
+    php ./composer.phar require sammaye/yii2-abtest:"@stable"
+
+## Adding it to your configuration
+
+Easiest thing is to just shown example of my configuration:
+
+	'test' => [
+		'class' => 'sammaye\abtest\Test',
+		'filter' => [
+			'rules' => [
+				[
+					'allow' => false,
+					'roles' => ['staff']
+				]
+			]
+		],
+		'tests' => [
+			[
+				'name' => 'Beta Search',
+				'values' => ['old', 'new'],
+				'default' => 'new'
+			]
+		]
+	],
+	
+The configuration breaks down into two parameters:- `filter` and `tests`.
+
+**Only `tests` is required.**
+
+`filter` allows you to use the `AccessControl` like you would on any controller and 
+provide a set of rules whereby the tests should not take effect.
+
+When an `'allow' => false` filter is matched the test will return the `default` parameter from the test you are looking at 
+and will not not record it in `$_SESSION`.
+
+This way, when you come to `list()` at the end of the page to push onwards for analytics, these tests **will not appear** in that list.
+
+## Using it
+
+Once it is fully configured you can just use it to detect which path a user takes:
+
+    Yii::$app->test->value('Beta Search')
+    
+This function will return either `old` or `new` depending on whether I am a staff member or lady random chooses me.
+
+In order to add it to your analytics code you need to list all active tests and their values:
+
+    Yii::$app->test->list()
+    
+This will then print out a list of (for me):
+
+    [
+        'Old Search' => 'new'
+    ]
+
+So, the key is name of the test and the value is the selected variant of the test.
---
+++ Test.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace sammaye\abtest;
+
+use Yii;
+use yii\base\Component;
+use yii\base\Exception;
+use yii\filters\AccessControl;
+use yii\helpers\Inflector;
+
+class Test extends Component
+{
+    public $filter;
+    public $tests;
+    
+    public function prefix()
+    {
+        return 'yii2_split_test_';
+    }
+    
+    public function key($v)
+    {
+        return $this->prefix() . Inflector::slug($v, '_');
+    }
+
+    public function value($name)
+    {
+        if(!$test = $this->getTest($name)){
+            throw new Exception('Test "' . $name . '" is not in configuration');
+        }
+        
+		$session = Yii::$app->getSession();
+		$key = $this->key($name);
+		
+		if(!isset($test['values']) || !is_array($test['values'])){
+            throw new Exception('Test "' . $name . '" requires an array of values');
+		}
+		$values = $test['values'];
+
+        $filter = isset($test['filter']) ? $test['filter'] : $this->filter;
+        if($filter){
+            if(!isset($test['default'])){
+                throw new Exception(
+                    'Test "' . $name . '" requires a "default" parameter and value'
+                );
+            }
+            
+            // Add a default rule for allowing it all
+            $filter['rules'][] = ['allow' => true];
+            
+            $o = Yii::createObject(
+                array_merge(
+                    [
+                        'class' => AccessControl::class, 
+                    ], 
+                    $filter
+                )
+            );
+            if(!$this->can($o)){
+                return $test['default'];
+            }
+        }
+
+		$active = $session->get($key);
+		
+		if(!$active || !in_array($active, $values)){
+		    // Pick a test randomly
+			$active = $values[array_rand($values)];
+			$session->set($key, [$name, $active]);
+		}
+		
+		return $active;
+    }
+    
+    public function list()
+    {
+        $session = Yii::$app->getSession();
+        $list = [];
+        foreach($this->tests as $k => $v){
+            if(!isset($v['name'])){
+                continue;
+            }
+            $name = $v['name'];
+            
+            $sessionTest = $session->get($this->key($name));
+            if($sessionTest && is_array($sessionTest)){
+                $list[$name] = $sessionTest[1];
+            }
+        }
+        return $list;
+    }
+    
+    public function getTest($name)
+    {
+        foreach($this->tests as $k => $v){
+            if(isset($v['name']) && $v['name'] === $name){
+                return $v;
+            }
+        }
+    }
+    
+    public function can($ac)
+    {
+        $user = $ac->user;
+        $request = Yii::$app->getRequest();
+        /* @var $rule AccessRule */
+        foreach($ac->rules as $rule){
+            if($allow = $rule->allows(Yii::$app->controller->action, $user, $request)){
+                return true;
+            }elseif($allow === false){
+                return false;
+            }
+        }
+        return false;
+    }
+}
